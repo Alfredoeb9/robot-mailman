28a06be12eefa99984a38a08225086d10537a11b
Alfredoeb9 <alfredoeb96@gmail.com>
2024-05-01 12:54:23 -0700

    [robot] robot delivers parcels to address

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 const roads = require("./data");
 
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+
+  move(destination) {
+    // check if there is no road going from current place to destination
+    // return old state else return updated path
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      // the map takes care of the moving
+      // the filter takes care of the delivering
+      let parcels = this.parcels
+        .map((p) => {
+          if (p.place != this.place) return p;
+          return { place: destination, address: p.address };
+        })
+        .filter((p) => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
+}
+
+/* buildGraphy creates a map object that, for each node,
+   stores an array of connected notes.
+*/
 function buildGraph(edges) {
   let graph = Object.create(null);
   function addEdge(from, to) {
@@ -10,6 +38,7 @@ function buildGraph(edges) {
     }
   }
 
+  // loop through edges/roads but split through the - as past the dash is the from -> to relation
   for (let [from, to] of edges.map((r) => r.split("-"))) {
     addEdge(from, to);
     addEdge(to, from);
@@ -19,4 +48,41 @@ function buildGraph(edges) {
 
 const roadGraph = buildGraph(roads.roads);
 
-console.log(roadGraph);
+function runRobot(state, robot, memory) {
+  for (let turn = 0; ; turn++) {
+    if (state.parcels.length === 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+
+function randomRobot(state) {
+  return { direction: randomPick(roadGraph[state.place]) };
+}
+
+VillageState.random = function (parcelCount = 5) {
+  let parcels = [];
+  for (let i = 0; i < parcelCount; i++) {
+    let address = randomPick(Object.keys(roadGraph));
+    let place;
+
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while (place === address);
+    parcels.push({ place, address });
+  }
+  return new VillageState("Post Office", parcels);
+};
+
+// run function
+runRobot(VillageState.random(), randomRobot);
